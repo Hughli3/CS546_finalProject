c074ca7c611a003f1def461e514ce6a13a0c7b4e
nealxinyu <nealxinyu@gmail.com>
2019-11-18 19:28:52 -0500

    update structure

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const app = express();
+const exphbs = require('express-handlebars');
+const static = express.static(__dirname + '/public');
 const configRoutes = require('./routes');
+const app = express();
+
 app.use(express.json());
+app.use('/public', static);
+app.use(express.urlencoded({extended: true}));
+
 configRoutes(app);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
-const userRoutes = require('./users');
-const dogRoutes = require('./dogs');
-
 const constructorMethod = (app) => {
-  app.use('/user', userRoutes);
-  app.use('/dog', dogRoutes);
+  app.get('', async (req, res) => {
+    res.render('home', {title: "Home"});
+  });
+
+  app.get('/dogs', async (req, res) => {
+    res.render('dogs/dogs', {title: "All Dogs"});
+  });
+
+  app.get('/login', async (req, res) => {
+    res.render('login', {title: "Login"});
+  });
+
+  app.get('/signup', async (req, res) => {
+    res.render('signup', {title: "Signup"});
+  });
+
+  app.get('/profile', async (req, res) => {
+    res.render('profile', {title: "Profile"});
+  });
+
+  app.get('/dog/:id', async (req, res) => {
+    res.render('dogs/single_dog', {title: "Single Dog"});
+  });
 
   app.use('*', (req, res) => {
     res.status(404).json({error: 'Not found'});
   });
 };
+
 
 module.exports = constructorMethod;
